a746ffdce8193ae72a4701088f20099351a72131
Freedy <21914103@zju.edu.cn>
2020-05-02 09:58:38 +0800

    加上了原型

--- src/jQuery.js
+++ src/jQuery.js
@@ -1,24 +1,36 @@
-window.jQuery = function (selectorOrArray) {
+window.$ = window.jQuery = function (selectorOrArray) {
     let elements
     if (typeof selectorOrArray === 'string') {
         elements = document.querySelectorAll(selectorOrArray)
     } else if (selectorOrArray instanceof Array) {
         elements = selectorOrArray
     }
-    // api:可以操作elements的对象
-    return {//省去了声明api的步骤，直接返回这个对象
-        // 闭包：函数访问外部变量，addClass访问了elements这个变量，去维持这个elements
-        oldApi: selectorOrArray.oldApi,//因为刚才oldApi是加在数组的身上的，但是下面调用的时候用的是api，所以要把这个属性加在api上 
+    const api = Object.create(jQuery.prototype)//创建一个对象，这个对象的__proto__为括号里的东西，相当于 const api = {__proto__:jQuery.prototype}
+    // // api:可以操作elements的对象
+    // api.elements = elements
+    // api.oldApi = selectorOrArray.oldApi
+    Object.assign(api, {
+        elements = elements,
+        oldApi = selectorOrArray.oldApi
+    })// 用原型节约了内存
+    return api//省去了声明api的步骤，直接返回这个对象
+    // 闭包：函数访问外部变量，addClass访问了elements这个变量，去维持这个elements
+    //因为刚才oldApi是加在数组的身上的，但是下面调用的时候用的是api，所以要把这个属性加在api上 
+
+
+    jQuery.prototype = {
+        constructor: jQuery,
+        jQuery: true,
         addClass(className) {
-            for (let i = 0; i < elements.length; i++) {
-                elements[i].classList.add(className)
+            for (let i = 0; i < this.elements.length; i++) {
+                this.elements[i].classList.add(className)
             }
             return this // 因为api没有被声明，所以这里用this很精髓
         },
         find(selector) {
             let array = []
-            for (let i = 0; i < elements.length; i++) {
-                array.push(element[i].querySelectorAll(selector))
+            for (let i = 0; i < this.elements.length; i++) {
+                array.push(this.element[i].querySelectorAll(selector))
             }
             array.oldApi = this // this就是api，这个this是之前的api，api1
             return jQuery(array) // 给我传什么，就会返回一个对象去操作它
@@ -27,8 +39,8 @@ window.jQuery = function (selectorOrArray) {
             return this.oldApi // 这个this是当前的api 即数组的api，api2
         },
         each(fn) {
-            for (let i = 0; i < elements.length; i++) {
-                fn.call(null, elements[i], i)
+            for (let i = 0; i < this.elements.length; i++) {
+                fn.call(null, this.elements[i], i)
             }
             return this// this就是当前的api
         },
@@ -42,7 +54,7 @@ window.jQuery = function (selectorOrArray) {
             return jQuery(array)//封装一个可以操作array的对象
         },
         print() {
-            console.log(elements)//这个elements就是this所操作的东西，对应着当前api所操作的当前的elements
+            console.log(this.elements)//这个elements就是this所操作的东西，对应着当前api所操作的当前的elements
         },
         children() {
             const array = []
--- src/main.js
+++ src/main.js
@@ -1,3 +1,11 @@
-const x = jQuery('.test')
+const div1 = document.querySelector('.test')//DOM对象
 
-x.children().print()
+const div2 = $('.test')//能够操作test的api
+
+DOM对象只能使用DOM的API
+jQuery对象只能使用jQuery的API
+
+所以在命名的时候DOM对象应该这样命名
+elDiv1 = document.querySelector('.test')
+在命名jQuery对象的时候应该这样命名
+$div2 = $('.test')
